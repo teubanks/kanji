8cd393c0d9cfa9a2a90321fe71e6b14623cbeda9
Tracey Eubanks <narshlob@Traceys-iMac.local>
2013-11-05 15:43:26 -0700

    Multiple lines for labels
    Kanji and english labels extend to multiple lines

--- kanji/KanjiViewController.m
+++ kanji/KanjiViewController.m
@@ -30,20 +30,21 @@
 {
     // Update the user interface for the detail item.
 
-  if (self.detailItem) {
-    self.kanjiLabel.text = [[self.detailItem valueForKey:@"kanji"] description];
-    self.onyomiLabel.text = [[self.detailItem valueForKey:@"onyomi"] description];
-    self.kunyomiLabel.text = [[self.detailItem valueForKey:@"kunyomi"] description];
-    self.englishLabel.text = [[self.detailItem valueForKey:@"english"] description];
-    
-  }
+    if (self.detailItem) {
+        self.kanjiLabel.text = [[self.detailItem valueForKey:@"kanji"] description];
+        self.onyomiLabel.text = [[self.detailItem valueForKey:@"onyomi"] description];
+        self.kunyomiLabel.text = [[self.detailItem valueForKey:@"kunyomi"] description];
+        self.kunyomiLabel.numberOfLines = 3;
+        self.englishLabel.text = [[self.detailItem valueForKey:@"english"] description];
+        self.englishLabel.numberOfLines = 2;
+    }
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-  [self configureView];
+    [self configureView];
 }
 
 @end
