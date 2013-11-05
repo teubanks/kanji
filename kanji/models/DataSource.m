5fdb62723e04c3b4c7f397a67e88a52d781799e1
Tracey Eubanks <narshlob@Traceys-iMac.local>
2013-11-04 22:22:29 -0700

    UI Formatting
    made things spaced more feng shui-ish
    Put radicals first, then JLPT level 5

--- kanji/models/DataSource.m
+++ kanji/models/DataSource.m
@@ -25,20 +25,20 @@ NSString *radicalHeaderID = @"RadicalHeader";
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     if(section == 0){
-        return [Kanji numberOfKanjisInContext:self.managedObjectContext];
-    } else {
         return [Radical numberOfRadicalsInContext:self.managedObjectContext];
+    } else {
+        return [Kanji numberOfKanjisInContext:self.managedObjectContext];
     }
 }
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     if(indexPath.section == 0){
-        KanjiCell *cell = (KanjiCell*)[collectionView dequeueReusableCellWithReuseIdentifier:kanjiCellID forIndexPath:indexPath];
-        [cell.kanjiCellLabel setText:@"kanji"];
-        return cell;
-    } else {
         RadicalCell *cell = (RadicalCell*)[collectionView dequeueReusableCellWithReuseIdentifier:radicalCellID forIndexPath:indexPath];
         [cell.radicalCellLabel setText:@"rad"];
+        return cell;
+    } else {
+        KanjiCell *cell = (KanjiCell*)[collectionView dequeueReusableCellWithReuseIdentifier:kanjiCellID forIndexPath:indexPath];
+        [cell.kanjiCellLabel setText:@"kanji"];
         return cell;
     }
 }
@@ -48,9 +48,9 @@ NSString *radicalHeaderID = @"RadicalHeader";
     if(kind == UICollectionElementKindSectionHeader){
         sectionHeader = (HeaderView*)[collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:kanjiHeaderID forIndexPath:indexPath];
         if(indexPath.section == 0){
-            [sectionHeader.headerTitleLabel setText:@"JLPT Level 5"];
-        } else {
             [sectionHeader.headerTitleLabel setText:@"Radicals"];
+        } else {
+            [sectionHeader.headerTitleLabel setText:@"JLPT Level 5"];
         }
     }
     return sectionHeader;
